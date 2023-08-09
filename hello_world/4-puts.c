36fccec58defdafe7741dc1e98fb82557db26670
Godfrey <mpoly702@gmail.com>
2023-08-09 22:09:15 +0100

    g

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - This program uses put()
+ *  Description - This program prints out a sentence to output
+ *  Return: 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,\n");
+return (0);
+}
