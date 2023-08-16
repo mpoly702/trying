d2a04921aa505e67c915c074bf004a9698824b2e
Godfrey <mpoly702@gmail.com>
2023-08-16 09:01:17 -0700

    gil

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Hello Godfrey\n");
+	printf("Hello to you too");
+	return (0);
+}
