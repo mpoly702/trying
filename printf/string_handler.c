c61ad925dbae1efe32d1df853e0b875a788a50b8
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-20 17:54:01 +0100

    Update string_handler.c

--- printf/string_handler.c
+++ printf/string_handler.c
@@ -8,12 +8,13 @@
 
 int print_String(va_list val) 
 {
+    int i;
     char *s = va_arg(val, char *);
-    int len = (s != NULL) ? _strlen(s) : 6; // Default to "(null)" length if s is NULL
-
-    for (int i = 0; i < len; i++) {
+    /*Default to "(null)" length if s is NULL*/
+    int len = (s != NULL) ? _strlen(s) : 6;
+    for (i = 0; i < len; i++) 
+    {
         _putchar((s != NULL) ? s[i] : "(null)"[i]);
     }
-
     return len;
 }
