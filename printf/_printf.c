c984e6c0871f38cedd94e18ada2f397cffae1a89
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-20 17:39:04 +0100

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
-typedef int (*PrintfFunction)(va_list);
 
-typedef struct {
-    const char *id;
-    PrintfFunction f;
-} ConvertMatch;
 
 int _printf(const char *const format, ...) {
     ConvertMatch m[] = {
-        {"%s", printf_string}, {"%c", printf_char},
-        {"%%", printf_37}, {"%i", printf_int},
-        {"%d", printf_dec}, {"%r", printf_srev},
-        {"%R", printf_rot13}, {"%b", printf_bin},
-        {"%u", printf_unsigned}, {"%o", printf_oct},
-        {"%x", printf_hex}, {"%X", printf_HEX},
-        {"%S", printf_exclusive_string}, {"%p", printf_pointer}
+        {"%s", print_string}, {"%c", print_Char},
+        {"%%", print_percent}
     };
 
     va_list args;
