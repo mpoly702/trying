7473fb7688b3b276b40d0ecb9e3b7ce07ab97979
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-22 15:54:38 +0100

    Create functions_3.c

---
+++ printf/safe_haven/chatgpt3.0/functions_3.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+int print_hexadecimal(va_list args, pmtrs_t *pmtrs) {
+    unsigned long li;
+    char *str;
+    (void)pmtrs;
+
+    if (pmtrs->modi_l)
+        li = (unsigned long)va_arg(args, unsigned long);
+    else if (pmtrs->modi_h)
+        li = (unsigned short int)va_arg(args, unsigned int);
+    else
+        li = (unsigned int)va_arg(args, unsigned int);
+
+    str = transform(li, 16, 2, pmtrs);
+
+    if (pmtrs->flg_htag && li) {
+        *--str = 'x';
+        *--str = '0';
+    }
+
+    pmtrs->usg = 1;
+    return _puts(str);
+}
+
+int print_octal(va_list args, pmtrs_t *pmtrs) {
+    unsigned long li;
+    char *str;
+    (void)pmtrs;
+
+    if (pmtrs->modi_l)
+        li = (unsigned long)va_arg(args, unsigned long);
+    else if (pmtrs->modi_h)
+        li = (unsigned short int)va_arg(args, unsigned int);
+    else
+        li = (unsigned int)va_arg(args, unsigned int);
+
+    str = transform(li, 8, 2, pmtrs);
+
+    if (pmtrs->flg_htag && li)
+        *--str = '0';
+
+    pmtrs->usg = 1;
+    return _puts(str);
+}
+
+int print_unsigned(va_list args, pmtrs_t *pmtrs) {
+    unsigned long li;
+    
+    if (pmtrs->modi_l)
+        li = (unsigned long)va_arg(args, unsigned long);
+    else if (pmtrs->modi_h)
+        li = (unsigned short int)va_arg(args, unsigned int);
+    else
+        li = (unsigned int)va_arg(args, unsigned int);
+
+    pmtrs->usg = 1;
+    return _puts(transform(li, 10, 2, pmtrs));
+}
+
+int print_binary(va_list args, pmtrs_t *pmtrs) {
+    unsigned int x = va_arg(args, unsigned int);
+    char *str = transform(x, 2, 2, pmtrs);
+
+    if (pmtrs->flg_htag && x)
+        *--str = '0';
+
+    pmtrs->usg = 1;
+    return _puts(str);
+}
+i
