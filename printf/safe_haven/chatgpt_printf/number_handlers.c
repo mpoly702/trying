0cf157719c46eebbb74999b55f51089bf429142c
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-22 07:47:17 +0100

    Update number_handlers.c

--- printf/safe_haven/chatgpt_printf/number_handlers.c
+++ printf/safe_haven/chatgpt_printf/number_handlers.c
@@ -8,64 +8,71 @@ int print_w(char *begin, char *end, char *except)
     {
         if (begin != except)
         {
-            _putchar(*begin); // Output the character
-            add++;
+            char ch = *begin;
+            // Output the character
+            add += _putchar(ch);
         }
         begin++;
     }
-    return (add); // Return the number of characters displayed
+    return add;
 }
 
 /**comments here for next function**/
 
 int rev_str(va_list args, pmtrs_t *pmtrs)
 {
-    int li, add = 0;
+    int add = 0;
     char *str = va_arg(args, char *);
     (void)pmtrs;
 
     if (str)
     {
-        for (li = 0; *str; str++)
-            li++;
-        str--;
-        for (; li > 0; li--, str--)
+        // Calculate the length of the string
+        int len = 0;
+        while (str[len] != '\0')
         {
-            _putchar(*str); // Output the character in reverse order
-            add++;
+            len++;
+        }
+
+        // Output characters in reverse order
+        for (int i = len - 1; i >= 0; i--)
+        {
+            add += _putchar(str[i]);
         }
     }
-    return (add); // Return the number of bytes printed
+
+    return add;
 }
 
 /**comments here for next function**/
 
 int print_root(va_list args, pmtrs_t *pmtrs)
 {
-    int i, count = 0;
+    int i, indice;
+    int count = 0;
     char array[] = "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
     char *a = va_arg(args, char *);
     (void)pmtrs;
 
     i = 0;
+    indice = 0;
 
     while (a[i])
     {
         if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
         {
-            int index = (a[i] >= 'A' && a[i] <= 'Z') ? (a[i] - 'A') : (a[i] - 'a' + 26);
-            _putchar(array[index]); // Output ROT13 encoded character
-            count++;
+            if (a[i] >= 'A' && a[i] <= 'Z')
+                indice = a[i] - 'A';
+            else
+                indice = a[i] - 'a' + 26;
+
+            count += _putchar(array[indice]);
         }
         else
         {
-            _putchar(a[i]); // Output character as is
-            count++;
+            count += _putchar(a[i]);
         }
         i++;
     }
-    return (count); // Return the number of bytes printed
+    return count;
 }
-/**comments here for next function**/
-
-
