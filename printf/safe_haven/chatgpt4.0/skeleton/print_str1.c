1fab3403ffdef0a7a7813f518f847a1e9c15d482
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-22 21:44:56 +0100

    Create print_str1.c

---
+++ printf/safe_haven/chatgpt4.0/skeleton/print_str1.c
@@ -0,0 +1,40 @@
+/**
+* print_string - print the string
+* @ap: This is the argument pointer
+* @params: this is the structure of the parameter
+*
+* Return: The no character to be printed
+*/
+int print_str1(va_list args, pmtrs_t *pmtrs)
+{
+	char *str = va_arg(args, char *), pad_ch = ' ';
+	unsigned int padd = 0, add = 0, i = 0, j;
+	(void)pmtrs;
+	
+	switch ((int)(!str))
+	case 1:
+		str = STR_NIL;
+		j = padd = _strlen(str);
+		
+		if (pmtrs->precision < padd)
+			j = padd = pmtrs->precision;
+		if (pmtrs->flg_ms)
+		{
+			if (pmtrs->precision != UINT_MAX)
+				for (i = 0; i < padd; i++)
+					add += _putchar(*str++);
+			else
+				add += _puts(str);
+		}
+		while (j++ < pmtrs->width)
+			add += _putchar(pad_ch);
+		if (!pmtrs->flg_ms)
+		{
+			if (pmtrs->precision != UINT_MAX)
+				for (i = 0; i < padd; i++)
+					add += _putchar(*str++);
+			else
+				add += _puts(str);
+		}
+		return (add);
+}
