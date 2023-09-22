fa3163736cb2ab9dd4069275f40ee115e49793b7
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-22 22:11:13 +0100

    Create _Adigit.c

---
+++ printf/safe_haven/chatgpt4.0/skeleton/_Adigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - check char is digit
+* @c: char to check
+*
+* Return: 1 if digit,and return 0 if otherwise
+*/
+int _Adigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
