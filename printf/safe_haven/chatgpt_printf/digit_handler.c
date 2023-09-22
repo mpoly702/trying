9dcc56ab57a73ba7bde1d5edb2b07e69c90e6bed
Godfrey Oriazowan <mpoly702@gmail.com>
2023-09-22 07:05:02 +0100

    Update digit_handler.c

--- printf/safe_haven/chatgpt_printf/digit_handler.c
+++ printf/safe_haven/chatgpt_printf/digit_handler.c
@@ -8,7 +8,25 @@ void init_pmtrs(pmtrs_t *pmtrs)
     pmtrs->flg_z = 0;
     pmtrs->flg_ms = 0;
     pmtrs->width = 0;
-    pmtrs->precision = UINT_MAX;
+    pmtrs->precision = 4294967295U; // Equivalent to UINT_MAX
     pmtrs->modi_h = 0;
     pmtrs->modi_l = 0;
+}
+
+/**
+ * print_per - Print a '%' character
+ * @args: Argument pointer
+ * @pmtrs: Parameters structure
+ *
+ * Return: The number of characters printed (always 1)
+ */
+int print_per(va_list args, pmtrs_t *pmtrs)
+{
+    (void)args;
+    (void)pmtrs;
+
+    char percent = '%';
+    _putchar(percent);
+
+    return 1;
 }
