c30bbfdf45233dd97081b1f6baddbbacd44d42ee
Godfrey <mpoly702@gmail.com>
2023-09-22 14:49:55 +0100

    find

--- printf/safe_haven/chatgpt_printf1.0/number_handlers.c
+++ printf/safe_haven/chatgpt_printf1.0/number_handlers.c
@@ -19,12 +19,12 @@ int print_w(char *begin, char *end, char *except)
         if (begin != except)
         {
             char ch = *begin;
-            // Output the character
+            /*Output the character*/
             add += _putchar(ch);
         }
         begin++;
     }
-    return add;
+    return(add);
 }
 
 /**
@@ -41,23 +41,26 @@ int rev_str(va_list args, pmtrs_t *pmtrs)
     char *str = va_arg(args, char *);
     (void)pmtrs;
 
+
     if (str)
     {
-        // Calculate the length of the string
+        /*Calculate the length of the string*/
         int len = 0;
+	int i;
+
         while (str[len] != '\0')
         {
             len++;
         }
 
-        // Output characters in reverse order
-        for (int i = len - 1; i >= 0; i--)
+        /*Output characters in reverse order*/
+        for (i = len - 1; i >= 0; i--)
         {
             add += _putchar(str[i]);
         }
     }
 
-    return add;
+    return(add);
 }
 
 /**
