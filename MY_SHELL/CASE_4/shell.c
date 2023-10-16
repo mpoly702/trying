05785e96ec209ff0e3ffec9638e3132014352f6e
Godfrey Oriazowan <mpoly702@gmail.com>
2023-10-16 23:31:58 +0100

    Create shell.c
    shell.c

---
+++ MY_SHELL/CASE_4/shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
