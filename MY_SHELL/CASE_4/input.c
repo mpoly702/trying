418b3778094178d35fbcd94bed61cf3d4550fd39
Godfrey Oriazowan <mpoly702@gmail.com>
2023-10-16 23:36:06 +0100

    Create input.c

---
+++ MY_SHELL/CASE_4/input.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+}
