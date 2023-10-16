63c2f487e7097269fa7740a3b1db8e6810a4d11e
Godfrey Oriazowan <mpoly702@gmail.com>
2023-10-17 00:55:01 +0100

    Create main.c

---
+++ MY_SHELL/CASE_4/main.c
@@ -0,0 +1,21 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+
+
+
