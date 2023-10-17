f904bfdf91f27624809389b1e9d8ec730cbe57bf
Godfrey Oriazowan <mpoly702@gmail.com>
2023-10-17 05:47:47 +0100

    Update built_in function.c

--- MY_SHELL/CASE_4/built_in function.c
+++ MY_SHELL/CASE_4/built_in function.c
@@ -1,17 +1,23 @@
-void* findMatchingFunction(const char *str) {
-    // Define an array of struct pairs
+#include "shell.h"
+
+void* findMatchingFunction(char *str) 
+{
+    int i;
+    /*Define an array of struct pairs*/
     struct StringFunctionPair functionPairs[] = {
         {"A", functionA},
         {"B", functionB},
         {"C", functionC}
     };
 
-    for (int i = 0; i < sizeof(functionPairs) / sizeof(functionPairs[0]); i++) {
-        if (strcmp(functionPairs[i].str, str) == 0) {
+    for (i = 0; i < sizeof(functionPairs) / sizeof(functionPairs[0]); i++) 
+    {
+        if (strcmp(functionPairs[i].str, str) == 0) 
+        {
             return functionPairs[i].func;
         }
     }
-    return NULL; // Return NULL if no match is found
+    return NULL; /*Return NULL if no match is found*/
 }
 
 int main() {
@@ -27,9 +33,5 @@ int main() {
 
     return 0;
 }
-In this code, inputStrings[0] is directly passed to findMatchingFunction as the argument, and the rest of the code remains the same. It will work with the first string from the array for matching.
-
-
-
 
 
