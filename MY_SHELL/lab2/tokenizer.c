258a902fcab9310d8e967da617d6e70c7fd73a3b
Godfrey Oriazowan <mpoly702@gmail.com>
2023-10-18 22:09:52 +0100

    Create tokenizer.c

---
+++ MY_SHELL/lab2/tokenizer.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+char **tokenizer(char *string)
+{
+	int i = 0;
+	char *token = NULL;
+	char **args = NULL;
+	ssize_t BUFZONE = 0;
+
+	args = (char **)malloc(sizeof(char *) * BUF);
+	token = strtok(string, " ");
+
+	if (token == NULL)
+	{
+		perror("error:");
+		free(args);
+		return NULL;
+	}
+
+	while (token != NULL)
+	{
+		args[i] = token;
+		i++;
+
+		if (i >= BUF) 
+		{
+      			BUFZONE += BUF * 2;
+      			args = realloc(args, BUF * sizeof(char*));
+		}
+      		if (token == NULL)
+		{
+			perror("reallocation failed");
+			free(args);
+			return NULL;
+		}
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	return args;
+}
