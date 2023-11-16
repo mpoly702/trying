a408d3ca84a1db95fe1fde5455882c7b3fa6f87c
Godfrey Oriazowan <mpoly702@gmail.com>
2023-11-16 12:40:31 +0100

    Update util.c

--- MY_SHELL/labPATH/util.c
+++ MY_SHELL/labPATH/util.c
@@ -27,3 +27,31 @@ void removeLastDirectory(char *path)
     }
 }
 
+int cd_case_one(void)
+{
+   size_t pathSize;
+	char *cwd = NULL;
+
+	pathSize = 256;
+	cwd = (char *)malloc(pathSize);
+		 if (cwd == NULL)
+		 {
+        		perror("malloc");
+        		return (1);
+		 }
+		if (getcwd(cwd, pathSize) == NULL)
+		{
+        		perror("getcwd");
+        		free(cwd);
+			return (1);
+		}
+		removeLastDirectory(cwd);
+		if (chdir(cwd) == -1);
+		{
+			perror("chdir fail");
+			free(cwd);
+			return (1);
+		}
+      free(cwd);   
+		return (0);
+}
