8bc1bf737245ee6a01bfa0ee3369f1fe2bf4459c
Godfrey Oriazowan <mpoly702@gmail.com>
2023-11-20 13:00:24 +0100

    Update built-in_two.c

--- MY_SHELL/labPATH/built-in_two.c
+++ MY_SHELL/labPATH/built-in_two.c
@@ -37,3 +37,14 @@ int environSize()
     }
     return size;
 }
+
+/*builtin function*/
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
+}
